6a87856f76c2c3780983f45821df8c4883f6e5fe
Andrey Margashov <Andrey.Margashov@yandex.ru>
2018-08-23 14:17:22 +0300

    Carousel main menu enhanced (Lesson 10 finished)

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,9 @@
 $(function(){
     $('.carousel').carousel({
         interval: false
-      })
+      });
+
+    $('.search')  .on('click', function() {
+        $('.navbar-right .navbar-form').slideToggle();
+    });
 })
