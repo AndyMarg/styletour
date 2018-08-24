e4cb941204b3d28e809d71ff7375904626686343
Andrey Margashov <Andrey.Margashov@yandex.ru>
2018-08-24 13:51:25 +0300

    Sidebar exhanced (Lesson 14 finished)

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,4 +6,13 @@ $(function(){
     $('.search')  .on('click', function() {
         $('.navbar-right .navbar-form').slideToggle();
     });
-})
+});
+
+$(window).load(function() {
+    var carousecaptionWidth = $('#carousel-sidebar .active img').width();
+    $('carousel-sidebar img').each(function() {
+        $(this).attr('width', carousecaptionWidth);
+    });
+    $('#carousel-sidebar .sidebar-carousel-caption').css('max-width', carousecaptionWidth + 'px');
+    $('#carousel-sidebar .carousel-indicators').css('max-width', carousecaptionWidth + 'px');
+});
